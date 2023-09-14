4902439f6e91675bfd8f2808a9d447e0c2477e5a
Vitor Lany <101216359+vitorlany@users.noreply.github.com>
2023-09-14 10:19:09 -0300

    docker

--- implementacao/src/main/java/com/laboratorio/projeto/domain/Agente.java
+++ implementacao/src/main/java/com/laboratorio/projeto/domain/Agente.java
@@ -1,10 +1,18 @@
 package com.laboratorio.projeto.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
+import org.checkerframework.checker.signature.qual.Identifier;
 
+@Entity
 @Data
 public class Agente {
+    @Id
+    @Column(unique = true)
+    String cnpj;
+
     String nome;
     String endereco;
-    String cnpj;
 }
